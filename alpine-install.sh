72b30a1695da5d1cd00c100e64e0a139373453e0
Jerry <82269916@qq.com>
2021-04-12 00:45:26 +0800

    Remove useless files

--- alpine-install.sh
+++ alpine-install.sh
@@ -4,16 +4,24 @@ cd /
 
 init() {
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
-    apk --no-cache add zsh git curl tree zip vim lsof wget sysstat strace tcpdump net-tools bind-tools mariadb-client redis
+    apk add zsh git curl tree zip vim lsof wget sysstat strace tcpdump net-tools bind-tools
+    apk add mariadb-client redis php php-fpm nginx
 }
 
 install_zsh() {
     echo "install_zsh start"
 
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # retry
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+    if [ ! -d "/root/.oh-my-zsh" ];then
+        # retry
+        git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+    fi
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+    #wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh && chmod +x install.sh
+    #./install.sh
+    #./install.sh
+    # rm -f ./install.sh
 
     if [ ! -d "/root/.oh-my-zsh" ];then
         echo "build fail, not found dir [/root/.oh-my-zsh]"
@@ -24,6 +32,7 @@ install_zsh() {
         echo "build fail, not found file [/root/.zshrc]"
         exit 2
     fi
+
     sed -i 's/robbyrussell/maran/g' ~/.zshrc
 
     echo "install_zsh end"
@@ -49,43 +58,46 @@ clean() {
     rm -f /*.apk
     rm -f /var/cache/apk/*.gz
 
-    rm -f /usr/bin/mysql_find_rows
-    rm -f /usr/bin/mysql_fix_extensions
-    rm -f /usr/bin/mysql_waitpid
-    rm -f /usr/bin/mysqlaccess
-    rm -f /usr/bin/mysqladmin
-    rm -f /usr/bin/mysqlcheck
-    rm -f /usr/bin/mysqldumpslow
-    rm -f /usr/bin/mysqlimport
-    rm -f /usr/bin/mysqlshow
+    # rm -f /usr/bin/mysql_find_rows
+    # rm -f /usr/bin/mysql_fix_extensions
+    # rm -f /usr/bin/mysql_waitpid
+    # rm -f /usr/bin/mysqlaccess
+    # rm -f /usr/bin/mysqladmin
+    # rm -f /usr/bin/mysqlcheck
+    # rm -f /usr/bin/mysqldumpslow
+    # rm -f /usr/bin/mysqlimport
+    # rm -f /usr/bin/mysqlshow
 
-    rm -f /usr/bin/mariadb-access
-    rm -f /usr/bin/mariadb-admin
-    rm -f /usr/bin/mariadb-check
-    rm -f /usr/bin/mariadb-dumpslow
-    rm -f /usr/bin/mariadb-find-rows
-    rm -f /usr/bin/mariadb-fix-extensions
-    rm -f /usr/bin/mariadb-import
-    rm -f /usr/bin/mariadb-show
-    rm -f /usr/bin/mariadb-waitpid
+    # rm -f /usr/bin/mariadb-access
+    # rm -f /usr/bin/mariadb-admin
+    # rm -f /usr/bin/mariadb-check
+    # rm -f /usr/bin/mariadb-dumpslow
+    # rm -f /usr/bin/mariadb-find-rows
+    # rm -f /usr/bin/mariadb-fix-extensions
+    # rm -f /usr/bin/mariadb-import
+    # rm -f /usr/bin/mariadb-show
+    # rm -f /usr/bin/mariadb-waitpid
 
-    rm -f /usr/bin/zipcloak
-    rm -f /usr/bin/zipgrep
-    rm -f /usr/bin/zipinfo
-    rm -f /usr/bin/zipnote
-    rm -f /usr/bin/zipsplit
+    # rm -f /usr/bin/zipcloak
+    # rm -f /usr/bin/zipgrep
+    # rm -f /usr/bin/zipinfo
+    # rm -f /usr/bin/zipnote
+    # rm -f /usr/bin/zipsplit
 
-    rm -f /usr/bin/redis-benchmark
-    rm -f /usr/bin/redis-check-aof
-    rm -f /usr/bin/redis-check-rdb
-    rm -f /usr/bin/redis-sentinel
-    rm -f /usr/bin/redis-server
+    # rm -f /usr/bin/redis-benchmark
+    # rm -f /usr/bin/redis-check-aof
+    # rm -f /usr/bin/redis-check-rdb
+    # rm -f /usr/bin/redis-sentinel
+    # rm -f /usr/bin/redis-server
 
-    rm -f /usr/bin/tcpdump.4.99.0
+    # rm -f /usr/bin/tcpdump.4.99.0
 
-    rm -f /usr/bin/git-*
+    # rm -f /usr/bin/git-*
 
-    rm -f /usr/bin/myisam_ftdump
+    # rm -f /usr/bin/myisam_ftdump
+
+    cp /bin/zsh /usr/local/bin/sh
+    cp /bin/zsh /usr/local/bin/bash
 
     echo "clean end"
 }
