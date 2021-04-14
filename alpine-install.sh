d2643e7a680bd00c8eaf8f42a67bdd931a291414
Jerry <82269916@qq.com>
2021-04-14 15:13:49 +0800

    Remove useless files

--- alpine-install.sh
+++ alpine-install.sh
@@ -5,7 +5,7 @@ pwd
 init() {
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
     apk add zsh git curl tree zip vim lsof wget sysstat strace tcpdump net-tools bind-tools
-    apk add mariadb-client redis php php-fpm nginx
+    apk add mariadb-client redis php php-fpm nginx go openjdk11
 }
 
 install_zsh() {
